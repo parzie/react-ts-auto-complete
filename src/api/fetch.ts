499122d478d996e50588dab678e052dbc4f32249
Parzifal <plclavijop@gmail.com>
2024-01-05 12:29:29 -0500

    Cache Response
    - Cache response in memory to avoid call API again in the same session.

--- src/api/fetch.ts
+++ src/api/fetch.ts
@@ -1,11 +1,18 @@
 import { PokemonList } from "../Interface";
 
 const BASE_URL = 'https://pokeapi.co/api/v2/';
+const POKEMONS = 'pokemons';
+const dataCacheMap: Map<string, PokemonList | undefined> = new Map();
 
 export const getPokemons = async (limit = 10000, offset = 0) => {
-  const data = await fetch(`${BASE_URL}pokemon?limit=${limit}&offset=${offset}`)
-    .then(response => response.json())
-    .catch((error) => console.error(error)) as PokemonList | undefined;
+  if (!dataCacheMap.has(POKEMONS)) {
+    const data = await fetch(`${BASE_URL}pokemon?limit=${limit}&offset=${offset}`)
+      .then(response => response.json())
+      .catch((error) => console.error(error)) as PokemonList | undefined;
 
-  return data;
+    dataCacheMap.set(POKEMONS, data);
+  }
+
+  return dataCacheMap.get(POKEMONS);
 };
+
